b8a9767cae9a4a67d4fc862fc0078cfab513ec7e
teng <344396262@qq.com>
2018-08-28 17:39:33 +0800

    Static Project

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
+    <FootNav/>
   </div>
 </template>
 
 <script>
+import FootNav from './components/FootNav'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    FootNav
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/FootNav.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="list-footer">
+    <ul>
+      <li>
+        <router-link to="/home" class="link">
+          <i class="icon iconfont icon-shouye"></i>
+          <p>首页</p>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/home" class="link">
+          <i class="icon iconfont icon-leibieguanli"></i>
+          <p>类别</p>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/home" class="link">
+          <i class="icon iconfont icon-gouwuche"></i>
+          <p>购物车</p>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/home" class="link">
+          <i class="icon iconfont icon-ren"></i>
+          <p>我的</p>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FootNav',
+  methods: {
+    goTo(){
+      alert();
+    }
+  }
+}
+</script>
+
+<style scoped>
+  @import "../common/css/reset.css";
+  @import "../common/css/iconfont.css";
+  .list-footer {
+    position: relative;
+  }
+  .list-footer ul{
+    list-style: none;
+    position: fixed;
+    margin: 0;
+    padding: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    text-align: center;
+  }
+  .list-footer ul:after{
+    content: "";
+    display: block;
+    clear: both;
+  }
+  .list-footer ul li{
+    float: left;
+    background: #226bbb;
+    width: 25%;
+    height: 2.5rem;
+    position: relative;
+  }
+
+  .link{
+    text-decoration: none;
+    padding: 1rem;
+    color: #fff;
+    text-align: center;
+  }
+  .link i{
+    text-align: center;
+    font-size: 1rem;
+    position: absolute;
+    left: 50%;
+    top:.4rem;
+    margin-left: -.5rem;
+  }
+  .link p{
+    margin-top: .1rem;
+    font-size: 0.5rem;
+  }
+</style>
